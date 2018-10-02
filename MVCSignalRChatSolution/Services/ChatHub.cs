b12fd32ee4a3a74dda6f2348491da0ea1d5d2968
rafael acosta <infocrom.web@gmail.com>
2018-10-02 15:07:20 +0100

    Modificaciones en la interfáz gráfica y mejoras en el código javascript SignalR.

--- MVCSignalRChatSolution/Services/ChatHub.cs
+++ MVCSignalRChatSolution/Services/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -13,9 +14,23 @@ namespace MVCSignalRChatSolution.Services
         //    Clients.All.hello();
         //}
 
-        public void Send(string name, string message)
+        public void Send(string name, string message, string connID)
         {
-            Clients.All.sendChat(name, message);
+            // sendChat --> mismo nombre que en JavaScript
+            //Clients.All.sendChat(name, message);
+            Clients.All.sendChat(name, message, connID);
+        }
+
+        // Cuando un Usuaerio se Conecta
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        // Cuando un Usuario se Desconecta
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
