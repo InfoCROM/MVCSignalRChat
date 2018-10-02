6913c9740124b180f99e811e8f3ca38a9f64a05e
rafael acosta <infocrom.web@gmail.com>
2018-10-02 10:26:27 +0100

    Instalación de SignalR en el proyecto.

--- MVCSignalRChatSolution/App_Start/BundleConfig.cs
+++ MVCSignalRChatSolution/App_Start/BundleConfig.cs
@@ -27,6 +27,8 @@ namespace MVCSignalRChatSolution
                       "~/Content/site.css",
                       "~/Content/chat.css"
                       ));
+
+
         }
     }
 }
---
+++ MVCSignalRChatSolution/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MVCSignalRChatSolution.Startup))]
+
+namespace MVCSignalRChatSolution
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+
+            // Iniciaizamos SignalR
+            app.MapSignalR();
+        }
+    }
+}
