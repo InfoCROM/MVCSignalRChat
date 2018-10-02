3917e706f719c2f738d8a9b548fef38c3a369f44
rafael acosta <infocrom.web@gmail.com>
2018-10-02 21:01:18 +0100

    Mejoras en la interfáz gráfica.

--- MVCSignalRChatSolution/App_Start/RouteConfig.cs
+++ MVCSignalRChatSolution/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace MVCSignalRChatSolution
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Chat",
+                url: "Chat",
+                defaults: new { controller = "Home", action = "Chat", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
