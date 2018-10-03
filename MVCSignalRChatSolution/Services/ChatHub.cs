20d30a4d3bc4dc00b817e471371f2d41e51833fb
rafael acosta <infocrom.web@gmail.com>
2018-10-03 13:24:54 +0100

    Cambios en estilos e interfáz de usuario.

--- MVCSignalRChatSolution/Services/ChatHub.cs
+++ MVCSignalRChatSolution/Services/ChatHub.cs
@@ -14,6 +14,8 @@ namespace MVCSignalRChatSolution.Services
         //    Clients.All.hello();
         //}
 
+
+
         public void Send(string name, string message, string connID)
         {
             // sendChat --> mismo nombre que en JavaScript
@@ -24,6 +26,12 @@ namespace MVCSignalRChatSolution.Services
         // Cuando un Usuaerio se Conecta
         public override Task OnConnected()
         {
+            var _name = Context.User.Identity.Name;
+
+
+
+
+            //Clients.All.newUser(Context.ConnectionId);
             return base.OnConnected();
         }
 
